89bbdb5f02984eeb49db0731f555d4f86591b7f2
Hafsa Rana <hafsamrana25@gmail.com>
2025-01-27 15:04:11 +0000

    add GET /api/articles endpoint

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { getEndpoints } = require("./controllers/endpoints.controller");
 const { getTopics } = require("./controllers/topics.controller");
-const { getArticle } = require("./controllers/articles.controller");
+const {
+  getArticle,
+  getAllArticles,
+} = require("./controllers/articles.controller");
 
 const app = express();
 
@@ -10,6 +13,8 @@ app.get("/api", getEndpoints);
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticle);
+
+app.get("/api/articles", getAllArticles);
 
 app.use((err, req, res, next) => {
   const { msg, status } = err;
