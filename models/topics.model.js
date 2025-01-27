8fbe2f54ba20708646030de70d45e15d558ced0f
Hafsa Rana <hafsamrana25@gmail.com>
2025-01-27 13:30:58 +0000

    update select topics function

--- models/topics.model.js
+++ models/topics.model.js
@@ -1,9 +1,8 @@
 const db = require("../db/connection");
-const { checkItemExists } = require("./utils");
 
 exports.selectTopics = () => {
   const sql = "SELECT * FROM topics;";
   return db.query(sql).then(({ rows }) => {
-    return checkItemExists(rows);
+    return rows;
   });
 };
