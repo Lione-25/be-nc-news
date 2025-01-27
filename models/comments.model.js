68586184caf9f1f87d39c537e7d4b68b0da54732
Hafsa Rana <hafsamrana25@gmail.com>
2025-01-27 22:29:42 +0000

    create insertComment function

--- models/comments.model.js
+++ models/comments.model.js
@@ -10,3 +10,29 @@ exports.selectComments = ({ article_id }) => {
     return checkItemExists(rows) || rows;
   });
 };
+
+exports.insertComment = ({ username, body }, { article_id }) => {
+  if (username === undefined || body === undefined) {
+    return Promise.reject({ code: 400 });
+  }
+  const sql1 = "SELECT * FROM users WHERE username = $1";
+  const sql2 = "SELECT * FROM articles WHERE article_id = $1";
+  return Promise.all([db.query(sql1, [username]), db.query(sql2, [article_id])])
+    .then((result) => {
+      const usernameData = result[0].rows;
+      const articleData = result[1].rows;
+      const sql = `INSERT INTO comments (author, body, article_id) 
+        VALUES ($1, $2, $3) 
+        RETURNING *;`;
+      const args = [username, body, article_id];
+
+      return (
+        checkItemExists(usernameData) ||
+        checkItemExists(articleData) ||
+        db.query(sql, args)
+      );
+    })
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
