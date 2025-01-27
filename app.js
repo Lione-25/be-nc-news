a4ca27406f5f381b9be12999bb9f6439f951a42c
Hafsa Rana <hafsamrana25@gmail.com>
2025-01-27 12:44:43 +0000

    add GET /api/topics endpoint

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
-
 const { getEndpoints } = require("./controllers/endpoints.controller");
+const { getTopics } = require("./controllers/topics.controller");
 
 const app = express();
 
 app.get("/api", getEndpoints);
+
+app.get("/api/topics", getTopics);
+
+app.use((err, req, res, next) => {
+  const { msg, status } = err;
+  if (msg && status) {
+    res.status(status).send({ msg });
+  } else {
+    next(err);
+  }
+});
 
 app.use((err, req, res, next) => {
   console.log("there is an error 500=======>\n", err);
