fb5ca042a6daa9a1bcbf9475d675a937a86e7723
Hafsa Rana <hafsamrana25@gmail.com>
2025-01-28 12:47:52 +0000

    create patchArticle function

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,6 +1,7 @@
 const {
   selectArticle,
   selectAllArticles,
+  updateArticle,
 } = require("../models/articles.model");
 
 exports.getArticle = ({ params }, res, next) => {
@@ -17,6 +18,16 @@ exports.getAllArticles = (req, res, next) => {
   selectAllArticles()
     .then((articles) => {
       res.status(200).send({ articles });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchArticle = ({ params, body }, res, next) => {
+  updateArticle(params, body)
+    .then((article) => {
+      res.status(200).send({ article });
     })
     .catch((err) => {
       next(err);
