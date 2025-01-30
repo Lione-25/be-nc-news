7334998fdc4cbac5e95b230fb2221210736069b7
Hafsa Rana <hafsamrana25@gmail.com>
2025-01-30 11:51:55 +0000

    create selectUser function to select user by username

--- models/users.model.js
+++ models/users.model.js
@@ -1,6 +1,13 @@
-const { sqlReturnTable } = require("./utils");
+const { sqlReturnTable, sqlReturnItem, checkValueExists } = require("./utils");
 
 exports.selectUsers = () => {
   const sql = "SELECT * FROM users;";
   return sqlReturnTable(sql);
+};
+
+exports.selectUser = ({ username }) => {
+  return checkValueExists({ username }).then(() => {
+    const sql = "SELECT * FROM users WHERE username = $1;";
+    return sqlReturnItem(sql, [username]);
+  });
 };
